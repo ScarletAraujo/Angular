d3477c00d3e553776d84cf74856d1d3b51a40a50
ScarletAraujo <scarlet.araujo@gmail.com>
2021-04-26 16:25:35 -0300

    Angular e-commerce

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
